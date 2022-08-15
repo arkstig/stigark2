5aafbcfb6930e9661f48b014e62b19047989c7d5
Stig Ark <stig.ark@gmail.com>
2022-08-15 22:17:58 +0200

    Light mode
    Startet

--- base_custom/src/App.js
+++ base_custom/src/App.js
@@ -1,3 +1,4 @@
+import { createContext, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Aside from './components/Aside'
 import Footer from './components/Footer'
@@ -10,26 +11,34 @@ import Home from './pages/Home'
 import Ommeg from './pages/Ommeg'
 import Portfolio from './pages/Portfolio'
 
+export const ThemeContext = createContext(null)
 export default function App() {
   // Write JavaScript, use Hooks, add state and more
+  const [theme, setTheme] = useState('light')
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === 'light' ? 'dark' : 'light'))
+  }
 
   return (
-    <>
-      <Navigation />
-      <ScrollToTop />
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/portfolio">
-            <Route index element={<Portfolio />} />
-            <Route path=":slug" element={<CurrentPortfolio />} />
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div id={theme}>
+        <Navigation />
+        <ScrollToTop />
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/portfolio">
+              <Route index element={<Portfolio />} />
+              <Route path=":slug" element={<CurrentPortfolio />} />
+            </Route>
+            <Route path="/ommeg" element={<Ommeg />} />
           </Route>
-          <Route path="/ommeg" element={<Ommeg />} />
-        </Route>
-      </Routes>
-      <Aside />
-      <Footer />
-    </>
+        </Routes>
+        <Aside />
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
   )
 }
