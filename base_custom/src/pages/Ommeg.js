dea89d573c5f31acf35bdafb108996b029831581
Stig Ark <stig.ark@gmail.com>
2022-10-01 17:03:50 +0200

    Updatet links
    ommeg

--- base_custom/src/pages/Ommeg.js
+++ base_custom/src/pages/Ommeg.js
@@ -23,13 +23,21 @@ export default function Ommeg() {
         vil bruke tiden min til å jobbe med hver dag, for min hobby er hva som
         gir meg glede.
       </p>
+
       <p>
         Min passion er å forvirkeliggjøre visjoner og å skape ting fra null til
         noe visuelt. Det gir meg stor glede å kunne utvikle meg hele tiden, lære
         meg mer, å føle på mestring og stolthet over det jeg skaper. Jeg har et
-        øye for estetikk, det kan man se i mine fotografier og design. Jeg
-        setter også stor pris på den berg og dalbanen som kommer med å jobbe med
-        problemløsning.
+        øye for estetikk, det kan man se i{' '}
+        <a
+          target="_blank"
+          href="https://www.instagram.com/stigark"
+          rel="noreferrer"
+        >
+          mine fotografier
+        </a>{' '}
+        og design. Jeg setter også stor pris på den berg og dalbanen som kommer
+        med å jobbe med problemløsning.
       </p>
       <p>
         For øvrigt vil jeg beskrive meg selv som lærevillig, detaljorientert,
@@ -41,7 +49,6 @@ export default function Ommeg() {
         og design for å utvikle. Jeg gleder meg til å jobbe og fordype meg
         videre innen webutvikling og alt som hører til.
       </p>
-      <div className="divOmmeg" />
       <div className="buttonOmmeg">
         <NavLink to="/contact">
           <button type="button">Kontakt meg!</button>
